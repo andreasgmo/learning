9e19a8442c5dce33c69d13ca78bdb85e72d03751
andreas <andreasmaloste@gmail.com>
2019-05-11 12:35:24 -0300

    encrypt and decrypt system

--- src/main/java/MyFirstArray.java
+++ src/main/java/MyFirstArray.java
@@ -1,6 +1,6 @@
 public class MyFirstArray {
     public static void main(String[] args){
-        String needToBeReversed = "nothing to see here";
+        String needToBeReversed = ".oiem od airótaderp edadivita amu é opmirag o etneibma oiem o otiefer rof oãn eS .ahcor an sadavacse sairelag me adnia uo ,setnarolfa sadazilarenim sahcor uo seõivula son otreba uéc a adivlovnesed res edop opmirag od edadivita A .etneibma oiem od sairótaderp sadaredisnoc sacincét ed oãçazilitu a moc e otnemajenalp muhnen mes atief sezev satium é ,siamina uo/e siaunam ,socitámuenp ,socinâcem soiem rop ,sosoilav etnemlareg ,soirénim ed oãçarolpxe atsE\n ";
         System.out.println(needToBeReversed);
         String oth = ArrayExample.reverse(needToBeReversed);
         System.out.println(oth);
--- src/main/java/me/maloste/ComponentA.java
+++ src/main/java/me/maloste/ComponentA.java
@@ -3,12 +3,12 @@ package me.maloste;
 import me.maloste.abc.Y;
 
 public class ComponentA {
-    private void x(){
+    private int x(){
         Y a = new Y();
         String happy = a.batatinha();
 
-        int[] x = new int[]{1,2,3,5,7,3,54};
-
+        String[] x = new String[]{"hgg", "ojiuj"};
+        return 1;
     }
 }
 
